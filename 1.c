dda9354ca45061e3dbdb059c1242e5b10c087488
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-03 19:03:49 +0600

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+/*determinate  area of circle*/
+
+#include<stdio.h>
+
+#define pi 3.1416
+
+void main()
+{
+int R;
+float area;
+printf("enter the value of area:");
+scanf("%d",&R);
+area=pi*R*R
+print("%f",area);
+}
