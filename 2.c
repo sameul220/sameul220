0a880ccd77b995326cca1a11b684d67d10bf9f4f
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-10 21:50:49 +0600

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+/*determine area of rectangle*/
+
+#include<stdio.h>
+void main()
+{
+  int w,l,area;
+  printf("enter the value of w and l:\n");
+  scanf("%d%d",&w,&l);
+  area=w*l;
+  printf("the area of rectangle is :%d",area);
+}
