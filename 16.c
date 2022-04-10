3b13daabb0fa8f3da3a4d023909786c10d2a29b1
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-11 00:15:19 +0600

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+    int i,j, n;
+    printf("Enter row number: ");
+    scanf("%d", &n);
+    for(i = 0;i<n;i++)
+    {
+        for(j=n;j>row;j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
