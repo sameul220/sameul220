16ee49639b52a29459ee17f2bf935a1e598428b0
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-10 23:47:46 +0600

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,20 @@
+/// squre matrix shape of *
+
+#include<stdio.h>
+void main()
+{
+int i,j,n;
+  printf("enter a number\t");
+  scanf("%d",&n);
+  for (i=0;i<=n;i++)
+  {
+  for(j=0;j<=n;j++)
+  {
+    printf("* ");
+  }
+  
+  printf("\n");
+  }
+
+
+}
