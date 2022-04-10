bb033abf2cef1363ad6a1bcdcb110f02cac51a6f
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-10 22:23:45 +0600

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,18 @@
+/*show a multiplication  table*/
+#include<stdio.h>
+void main()
+{
+int S;
+  printf ("enter the number:");
+  scanf("%d",&S);
+  printf("%dx1=%d\n",S,S*1);
+  printf("%dx2=%d\n",S,S*2);
+  printf("%dx3=%d\n",S,S*3);
+  printf("%dx4=%d\n",S,S*4);
+  printf("%dx5=%d\n",S,S*5);
+  printf("%dx6=%d\n",S,S*6);
+  printf("%dx7=%d\n",S,S*7);
+  printf("%dx8=%d\n",S,S*8);
+  printf("%dx9=%d\n",S,S*9);
+  printf("%dx10=%d\n",S,S*10);
+}
