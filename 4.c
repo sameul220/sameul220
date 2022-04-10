a66ff0d30c6098ccfa9d088e636e3a87c517529b
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-10 22:07:35 +0600

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,10 @@
+/*chack the value of ascii charectar*/
+
+#include <stdio.h>
+void main()
+{
+  char c;
+  printf("enter an ascii value:");
+  scanf("%c",&c);
+  printf("the ascii value is %c=%d",c,c );
+}
