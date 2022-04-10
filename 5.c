7b6718228335cfb33630cb8cda16d64558612f7f
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-10 22:14:14 +0600

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,12 @@
+/*chack leap year or not using ternery operator*/
+#include<stdio.h>
+void main()
+{
+  int y;
+  printf("enter the year:");
+  scanf("%d",&y);
+  (y%4==o)?(printf("leap year")):(printf("not leap year"));
+                                         
+ }
+                                         
+  
