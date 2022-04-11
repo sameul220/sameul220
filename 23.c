eb401ffcf567eda8553c170f630a486d98ceb9e7
sameul220 <102908161+sameul220@users.noreply.github.com>
2022-04-11 07:55:28 +0600

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,32 @@
+///short arrey element in asscending order
+
+#include<stdio.h>
+void main()
+{
+int a[20],p,n,q,t;
+  printf("how many elements");
+  scanf("%d",&n);
+  printf("enter the elements");
+  for(p=0;p<n;p++){
+  scanf("%d",&a[p]);}
+  printf("elemnts are shorted in asscending way:");
+  for(p=0;p<n;p++){
+  for (q=p+1;q>=0;q++){
+  if(a[p]>a[q]){
+  t=a[p];
+   a[p]=a[q];
+    a[q]=t;
+  
+  }
+  
+  }
+  
+  }
+  
+   for(p=0;p<n;p++)
+   {
+   printf("%d",a[p]);
+   }
+
+
+}  
